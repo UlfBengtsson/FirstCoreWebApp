cccfeb5143c2749ce81a82e99681cf624ff48564
Ulf Bengtsson <ulf.bengtsson@lexicon.se>
2019-12-20 12:10:01 +0100

    added Sessions

--- FirstCoreWebApp/Startup.cs
+++ FirstCoreWebApp/Startup.cs
@@ -16,6 +16,17 @@ namespace FirstCoreWebApp
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDistributedMemoryCache();
+
+            services.AddSession(options =>
+            {
+                // Set a short timeout for easy testing.
+                options.IdleTimeout = TimeSpan.FromMinutes(10);
+                options.Cookie.HttpOnly = true;
+                // Make the session cookie essential
+                options.Cookie.IsEssential = true;
+            });
+
             services.AddMvc();// add MVC so we can use it
             //services.AddControllersWithViews();
         }
@@ -31,16 +42,27 @@ namespace FirstCoreWebApp
 
             //app.UseDefaultFiles();  //looks for index.html or default.html in wwwroot folder.
             app.UseStaticFiles();   //default opens up the wwwroot folder to be accessed.
-            
+
+            app.UseSession();
+            //app.UseHttpContextItemsMiddleware();// Core 3 update meesed this one up
+
             app.UseRouting();
 
             app.UseEndpoints(endpoints =>
             {
+                // Custom Routes
                 endpoints.MapControllerRoute(
                     name: "ReviewRoute",                                        //Name of Route rule
                     pattern: "TheReviews",                                      //Url to match
                     defaults: new { controller = "Reviews", action = "Index" }  //What Controller & Action to call
                     );
+
+                endpoints.MapControllerRoute(
+                    name: "CreateReviewRoute",                                        //Name of Route rule
+                    pattern: "WriteYourReview",                                      //Url to match
+                    defaults: new { controller = "Reviews", action = "Create" }  //What Controller & Action to call
+                    );
+
                 // special routes before default
                 endpoints.MapControllerRoute("default", "{controller=Home}/{action=Index}/{id?}");
             });
